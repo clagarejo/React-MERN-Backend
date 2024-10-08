7c18696e984b04f3fbb17f5da9f5de2f5d23e45a
Yan Lagarejo <clagarejo89@gmail.com>
2024-10-07 20:04:11 -0500

    another build

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const path = require( 'path' )
+const path = require('path')
 const express = require('express')
 const { dbConnection } = require('./database/config')
 const cors = require('cors')
@@ -14,23 +14,23 @@ dbConnection()
 app.use(cors())
 
 // Directorio publico
-app.use( express.static('public') )
+app.use(express.static('public'))
 
 // Lectura y parseo del body
-app.use( express.json() )
+app.use(express.json())
 
 //rutas
 // TODO auth // crear, login, renew
-app.use('/api/auth', require( './routes/auth' ))
+app.use('/api/auth', require('./routes/auth'))
 
 // TODO CRUD: Eventos
-app.use('/api/events', require( './routes/events' ))
+app.use('/api/events', require('./routes/events'))
 
-app.use('*', ( req, res ) => {
-    res.sendFile( path.join( __dirname, 'public/index.html'))
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
 // Escuchar las peticiones
-app.listen( process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log(`servidor corriendo en ${process.env.PORT}`)
 })
