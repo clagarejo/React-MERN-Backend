d1de4f58718ec43baf093210926b8d7024a3d9ec
Yan Lagarejo <clagarejo89@gmail.com>
2024-10-07 19:41:39 -0500

    build frontend

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require( 'path' )
 const express = require('express')
 const { dbConnection } = require('./database/config')
 const cors = require('cors')
@@ -24,6 +25,10 @@ app.use('/api/auth', require( './routes/auth' ))
 
 // TODO CRUD: Eventos
 app.use('/api/events', require( './routes/events' ))
+
+app.use('*', ( req, res ) => {
+    res.sendFile( path.join( __dirname, 'public/index.html'))
+})
 
 // Escuchar las peticiones
 app.listen( process.env.PORT, () => {
